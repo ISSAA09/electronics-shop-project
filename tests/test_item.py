7b1d44c3f672dc61aa034d9debfbb6f5de7e8455
issaa09 <sahfiyev29@gmail.com>
2023-07-11 09:15:38 +0300

    Initial

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+exmp_1 = Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price():
+    assert exmp_1.calculate_total_price() == 200000
+
+
+def test_apply_discount():
+    assert 10000*Item.pay_rate == exmp_1.price
+    exmp_1.pay_rate = 0.8
+    exmp_1.apply_discount()
+    assert exmp_1.price == 8000.0
