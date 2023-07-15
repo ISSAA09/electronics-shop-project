54c455a3f0d9a0c271c1dbf34a07091808bc5898
issaa09 <sahfiyev29@gmail.com>
2023-07-16 00:11:27 +0500

    Initial

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -12,15 +16,25 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.__class__.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < 10:
+            self.__name = name
+        elif len(name) > 10:
+            self.__name = name[:10]
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
@@ -31,5 +45,21 @@ class Item:
         """
         self.price *= self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        with open('../src/items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = row['price']
+                quantity = row['quantity']
+                cls(name, price, quantity)
 
+    @staticmethod
+    def string_to_number(str_num):
+        if "." in str_num:
+            return int(float(str_num))
+        else:
+            return int(str_num)
 
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,4 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
 from src.item import Item
 
 exmp_1 = Item("Смартфон", 10000, 20)
@@ -10,7 +9,20 @@ def test_calculate_total_price():
 
 
 def test_apply_discount():
-    assert 10000*Item.pay_rate == exmp_1.price
+    assert 10000 * Item.pay_rate == exmp_1.price
     exmp_1.pay_rate = 0.8
     exmp_1.apply_discount()
     assert exmp_1.price == 8000.0
+
+
+def test_name():
+    assert exmp_1.name == "Смартфон"
+    exmp_1.name = 'СуперСмартфон'
+    assert exmp_1.name == "СуперСмарт"
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
